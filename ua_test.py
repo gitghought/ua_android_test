1e390e1d483fcdad49db718dc83b8fae002ea9ef
gitghought <simpleedu.hello@gmail.com>
2017-02-28 19:28:47 +0800

    20170228：实现获取Device和EasyDevice

--- ua_test.py
+++ ua_test.py
@@ -1,13 +1,26 @@
-import os
-from com.android.monkeyrunner import MonkeyRunner,MonkeyDevice
-from com.android.monkeyrunner import MonkeyImage
-from com.android.monkeyrunner import By
+import sys
+from com.android.monkeyrunner import MonkeyRunner,MonkeyDevice,MonkeyImage
+from com.android.monkeyrunner.easy import EasyMonkeyDevice
+from com.android.monkeyrunner.easy import By
 
 class MyMonkeyRunner:
+	idSearchStr = 'id/top_view_search'
 	def getDevice(self):
-		dev = MonkeyRunner.waitForConnection()
-		return dev
+		self.dev = MonkeyRunner.waitForConnection()
+		return self.dev
+	
+	def getEasyDevice(self, dev):
+		self.eDevice = EasyMonkeyDevice(dev)
+		return self.eDevice
+	
+	def touchByID(self, id):
+		self.eDevice.touch(By.id(id), MonkeyDevice.DOWN_AND_UP)
 
 
 if __name__ == "__main__":
 	mmr = MyMonkeyRunner()	
+
+	dev = mmr.getDevice()
+	eDev = mmr.getEasyDevice(dev)
+
+	mmr.touchByID(mmr.idSearchStr)
