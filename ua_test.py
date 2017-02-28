49a321b545b48e2827dfb88c62fe21066bfb588e
gitghought <simpleedu.hello@gmail.com>
2017-02-28 20:15:53 +0800

    20170228:添加获取hierarchyviewer对象的方法

--- ua_test.py
+++ ua_test.py
@@ -5,6 +5,7 @@ from com.android.monkeyrunner.easy import By
 
 class MyMonkeyRunner:
 	idSearchStr = 'id/top_view_search'
+	idMainBarStr = 'id/main_bar'
 	def getDevice(self):
 		self.dev = MonkeyRunner.waitForConnection()
 		return self.dev
@@ -12,15 +13,27 @@ class MyMonkeyRunner:
 	def getEasyDevice(self, dev):
 		self.eDevice = EasyMonkeyDevice(dev)
 		return self.eDevice
+
+	def getHierarchyViewer(self):
+		self.hDevice = self.dev.getHierarchyViewer()
+		return self.hDevice
 	
 	def touchByID(self, id):
 		self.eDevice.touch(By.id(id), MonkeyDevice.DOWN_AND_UP)
-
+		
 
 if __name__ == "__main__":
 	mmr = MyMonkeyRunner()	
 
 	dev = mmr.getDevice()
 	eDev = mmr.getEasyDevice(dev)
+	hDev = mmr.getHierarchyViewer()
 
-	mmr.touchByID(mmr.idSearchStr)
+	#mmr.touchByID(mmr.idSearchStr)
+	memView = hDev.findViewById(mmr.idMainBarStr)
+
+	i = 0
+	for view in memView:
+		print "i = " + str(i)
+		print view
+		i=i+1
